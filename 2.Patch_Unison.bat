3b564b5c106eab6a4c04af2a375419902b3920e5
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2023-04-25 18:15:00 +0200

    More Windows Versions supported
    - All builds since 1703 Creators Update onwards should work unless there were API changes
    - Better implementation of the patching process

--- 2.Patch_Unison.bat
+++ 2.Patch_Unison.bat
@@ -1,5 +1,23 @@
-cd "%1"
-del AppxSignature.p7x AppxBlockMap.xml [Content_Types].xml
-rmdir -r AppxMetadata
-powershell -Command "(gc AppxManifest.xml) -replace '10.0.22621.0','10.0.19045.0' | Out-File "AppxManifest.xml"
-xcopy Assets "%1"\Assets\
+@echo off
+setlocal
+
+set directory=%~1
+if not defined directory (
+    echo Please drag and drop the WebPhonePackaging folder
+    exit /b 1
+)
+
+pushd %directory% || (
+    echo Could not change directory to %directory%.
+    exit /b 1
+)
+
+del /q AppxSignature.p7x AppxBlockMap.xml [Content_Types].xml
+
+if exist AppxMetadata (
+    rmdir /q /s AppxMetadata
+)
+
+powershell -Command "(gc ppxManifest.xml) -replace '10.0.22621.0','10.0.15063.0' | Out-File “AppxManifest.xml”
+
+popd
